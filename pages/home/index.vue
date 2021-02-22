c6f1f57bda713f4f9be7a6d10ac430ffbebe9db7
wanghan0321 <454118337@qq.com>
2021-02-22 22:33:50 +0800

    测试部署

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -3,7 +3,7 @@
 
         <div class="banner">
             <div class="container">
-                <h1 class="logo-font">conduit -- 老王测试</h1>
+                <h1 class="logo-font">conduit -- 老王测试 -- 测试部署</h1>
                 <p>A place to share your knowledge.</p>
             </div>
         </div>
